081173925748ba348691ab7351bbb12d20206aee
Britney Pellouchoud <britneypellouchoud@berkeley.edu>
2019-09-25 11:47:47 -0700

    Tried TrReader.java

--- hw3/TrReader/TrReader.java
+++ hw3/TrReader/TrReader.java
@@ -12,8 +12,30 @@ public class TrReader extends Reader {
      *  FROM.charAt(i) to TO.charAt(i), for all i, leaving other characters
      *  in STR unchanged.  FROM and TO must have the same length. */
     public TrReader(Reader str, String from, String to) {
-        // TODO: YOUR CODE HERE
+        this.str = str;
+        this.from = from;
+        this.to = to;
     }
+
+    public int read() throws IOException {
+        int r = str.read();
+        if (c == -1) {
+            return c
+        }
+        else {
+            return translate((char) c);
+        }
+    }
+
+    public int read(char[] f) throws IOException {
+        int x = str.read(cbuf);
+        for(int i = 0; i <= x + 1; i++) {
+            return n;
+        }
+    }
+
+    //read:str.read
+
 
     /* TODO: IMPLEMENT ANY MISSING ABSTRACT METHODS HERE
      * NOTE: Until you fill in the necessary methods, the compiler will
