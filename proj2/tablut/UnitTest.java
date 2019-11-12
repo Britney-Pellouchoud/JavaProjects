c5f276357086caa7c9ec281d60f1ceeff7f5113b
Britney Pellouchoud <britneypellouchoud@berkeley.edu>
2019-11-11 20:11:57 -0800

    More testing

--- proj2/tablut/UnitTest.java
+++ proj2/tablut/UnitTest.java
@@ -2,6 +2,9 @@ package tablut;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static tablut.Piece.EMPTY;
+import static tablut.Piece.KING;
+
 import ucb.junit.textui;
 
 /** The suite of all JUnit tests for the enigma package.
@@ -19,6 +22,30 @@ public class UnitTest {
     @Test
     public void dummyTest() {
         return;
+    }
+
+    @Test
+    public void inittest() {
+        Board testboard = new Board();
+        assert testboard.getallPieces().get(03).toString() == "B";
+    }
+
+    @Test
+    public void copytest() {
+        Board testboard = new Board();
+        Board modelboard = new Board();
+        Piece k = KING;
+        modelboard.setMoveLimit(5);
+        modelboard.makeMove(Square.sq(3,0), Square.sq(2,0));
+        modelboard.makeMove(modelboard.NTHRONE, Square.sq(5,5));
+        modelboard.makeMove(Square.sq(5, 0), Square.sq(6,0));
+        modelboard.makeMove(modelboard.THRONE, modelboard.NTHRONE);
+        assert modelboard.get(modelboard.NTHRONE) == KING;
+        assert modelboard.get(modelboard.THRONE) == EMPTY;
+        testboard.copy(modelboard);
+        assert testboard.get(testboard.NTHRONE) == KING;
+        assert testboard.get(testboard.THRONE) == EMPTY;
+        System.out.println(testboard);
     }
 
 }
